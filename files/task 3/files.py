82cc1c03cf3a42376bbf61ca89e0aaae9c80fe8d
Onemored <bigduck@mail.ru>
2024-12-27 19:11:46 +0500

    Перерыв

--- files/task 3/files.py
+++ files/task 3/files.py
@@ -1,5 +1,6 @@
 import os
 
+
 def reads_files():
     fds = sorted(os.listdir())
     files_list = []
@@ -8,7 +9,7 @@ def reads_files():
         if file.endswith('.txt'):
             file_name = file
             file_path = os.path.join(os.getcwd(), file)
-            with open(file_name, 'r', encoding='utf-8') as f:
+            with open(file_path, 'r', encoding='utf-8') as f:
                 info_file = {}
                 text_in_file = f.readlines()
                 count = len(text_in_file)
@@ -16,13 +17,24 @@ def reads_files():
             files_list.append(info_file)
     return files_list
 
+def write_file():
+    list_dictonaries = reads_files()
+    list_dictonaries = sorted(test, key=lambda x: x['lines_in_file'])
+    print(list_dictonaries)
+    # out_file = "rewrite_file.txt"
+    # file_path = os.path.join(os.getcwd(), file)
+    # with open(file_path, 'w', encoding='utf-8') as f_total:
+    #     f_total.write(path1 + '\n')
+    #     f_total.write(str(len(file1)) + '\n')
+    #     f_total.writelines(file1)
+    #     f_total.write('\n')
 
-test = reads_files()
-# print(test)
-# print(len(test))
-print(type(test))
+
+
+    return list_dictonaries
+
+
+test1 = reads_files()
+test = write_file()
 print(test)
-# test = sorted(test, key=lambda x: x['lines_in_file'])
-for file in test:
-    print(file)
-#         print(info)
+print(test1)
