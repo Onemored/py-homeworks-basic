f988d4c9b3cb7d52b872c4f2fe08de0a4b051298
Onemored <bigduck@mail.ru>
2024-12-27 18:54:32 +0500

    Еще лучше

--- files/task 3/files.py
+++ files/task 3/files.py
@@ -2,29 +2,27 @@ import os
 
 def reads_files():
     fds = sorted(os.listdir())
-    files_list = {}
-    i = 0
+    files_list = []
     for file in fds:
 
         if file.endswith('.txt'):
             file_name = file
             file_path = os.path.join(os.getcwd(), file)
-            info_about_file = list()
-            i += 1
             with open(file_name, 'r', encoding='utf-8') as f:
                 info_file = {}
                 text_in_file = f.readlines()
                 count = len(text_in_file)
                 info_file['name_file'], info_file['lines_in_file'], info_file['text'] = file_name, count, text_in_file
-            info_about_file.append(info_file)
-        files_list[i] = info_about_file
+            files_list.append(info_file)
     return files_list
 
 
 test = reads_files()
-# test = test.values()
+# print(test)
+# print(len(test))
+print(type(test))
+print(test)
 # test = sorted(test, key=lambda x: x['lines_in_file'])
 for file in test:
     print(file)
-    for info in test[file]:
-        print(info)
+#         print(info)
