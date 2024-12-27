dcc5f1473e29cd26de0d271cae4fb4318ba1aef5
Onemored <bigduck@mail.ru>
2024-12-27 15:52:44 +0500

    Что то есть

--- files/task 3/files.py
+++ files/task 3/files.py
@@ -1,5 +1,15 @@
 import os
 
+def count_lines_in_file():
+
+
+def text_from_files(f):
+    text_in_file = []
+    for line in f:
+        text_in_file.append(line)
+        print(text_in_file)
+    # return text_in_file
+
 
 def reads_files():
     fds = sorted(os.listdir())
@@ -12,17 +22,22 @@ def reads_files():
             file_path = os.path.join(os.getcwd(), file)
             info_about_file = list()
             i += 1
+            text_in_file = []
             with open(file_name, 'r', encoding='utf-8') as f:
                 info_file = {}
                 count = sum(1 for line in f)
-                info_file['name_file'], info_file['lines_in_file'] = file_name, count
+                for line in f:
+                    text_in_file.append(line)
+                info_file['name_file'], info_file['lines_in_file'], info_file['text'] = file_name, count, text_in_file
                 info_about_file.append(info_file)
 
         files_list[i] = info_about_file
+    # sorted_files_list = sorted(files_list, key=lambda x: x['lines_in_file'])
     return files_list
     # print(file_name)
     # print(count)
 
 
 test = reads_files()
+# test = sorted(test[1:], key=lambda x: x['lines_in_file'])
 print(test)
--- files/task 3/text.py
+++ files/task 3/text.py
@@ -1,9 +1,18 @@
-text = '123 321 228 1488'
-dict = dict()
-for i in range(len(text.split(' '))):
-    print(i)
-    for j in text.split(' '):
-        print(j)
-        dict[i] = j
+import os
 
+
+def read_cookbook():
+    file_name = os.path.join(os.getcwd(), '1.txt')
+    dict = []
+    with open(file_name, 'r', encoding='utf-8') as f:
+        for line in f:
+           dict.append(line)
+        return dict
+
+# config = [];
+# with open(filename) as file:
+#     for line in file:
+#         config.append(line.rstrip());
+
+dict = read_cookbook()
 print(dict)
